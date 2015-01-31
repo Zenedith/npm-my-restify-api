d77d1feb32dd99557a66b9a8d0ac4ce96e474be7
Zenedith <github@zenedith.mailgun.org>
2015-01-31 19:45:06 +0100

    Added errors

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ var meta = require('./lib/meta');
 var app = require('./lib/app');
 
 var exports = {};
+
+exports.error = {
+  InternalError: require('./lib/error/internalError').InternalError,
+  NotFoundError: require('./lib/error/notFoundError').NotFoundError,
+  BadRequestError: require('./lib/error/badRequestError').BadRequestError,
+  ForbiddenError: require('./lib/error/forbiddenError').ForbiddenError
+};
+
 exports.runServer = app.runServer;
 exports.VERSION = meta.VERSION;
 
